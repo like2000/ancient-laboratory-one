845721e8599c8efd9e0a495a87251d66af0c8387
Kevin Li <kli@cern.ch>
2020-09-21 20:50:59 +0200

    Move int to bigint.

--- backend/chronicles/models.py
+++ backend/chronicles/models.py
@@ -4,7 +4,7 @@ from backend import db
 class Chronicles(db.Model):
     __tablename__ = 'chronicles'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.BigInteger, primary_key=True)
     url = db.Column(db.String, primary_key=True)
     name = db.Column(db.String, primary_key=True)
     active = db.Column(db.Boolean, primary_key=True)
